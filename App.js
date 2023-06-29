825ec3eada0cd1e642a5195b81c65f6a8a918ee1
Ajinkya Jagtap <115774348+Ajinkya2356@users.noreply.github.com>
2023-06-30 00:41:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from "./Registration";
+function App() {
+  return (
+    <div className="App">
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
